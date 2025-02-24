7c38840458028e290b61f10c56ee68a01c3c21ed
Guilherme Sales <guilhermessales11@gmail.com>
2025-02-24 02:08:35 -0300

    new kernel version and add new drivers

--- kernel.h
+++ kernel.h
@@ -11,12 +11,18 @@ typedef char (*ptrFunc)(void);
 
 //defini��o da estrutura de um processo
 typedef struct {
+	//ponteiro para fun��o a ser executada
 	ptrFunc func;
+	//per�odo de reexecu��o da fun��o
+	int periodo;
+	//contador de tempo de cada processo
+	int deadline;	
 }process;
 
 //prot�tipos das fun��es do kernel
 char kernelInit(void);
 char kernelAddProc(process* newProc);
 void kernelLoop(void);// declara��o de ponteiro de fun��o
+void kernelTick(void);
 
 #endif
